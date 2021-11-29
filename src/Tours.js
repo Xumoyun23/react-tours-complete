bb15f86f0d2d86ad22a047dba7c973d8be49587b
Humoyun <xumoyunyarashev8124@.gmail.com>
2021-11-29 10:42:20 +0500

    first commit

---
+++ src/Tours.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Tour from './Tour';
+const Tours = ({tours}) => {
+ 
+  return (
+    <section>
+      <div className="title">
+        <h2>ours Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) =>{
+          return <Tour key={tour.id} {...tour} />
+        }
+        )}
+      </div>
+    </section>
+  )
+  
+};
+
+export default Tours;
